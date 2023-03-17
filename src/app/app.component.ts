87ea07c98af9e59c2032f93f8cdc5d3c3ae2a056
wesley castelo branco <wesleycastelobranco@192.168.1.4>
2023-03-17 02:42:47 -0300

    teste

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'msgpai';
+
+  texto: string = ''
+
+  constructor(private renderer: Renderer2, private el: ElementRef) { }
+
+  enviar() {
+    const iframeElement = this.el.nativeElement.querySelector('#meu-iframe');
+    const iframeContent = iframeElement.contentWindow;
+    iframeContent.postMessage(this.texto, '*');
+  }
 }
